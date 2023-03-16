c57a02a9cebe0888d253538122af2def42ebeec7
tonim04 <toni.marziliano@gmail.com>
2023-03-16 17:56:56 +0100

    home & film page

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,1 +1,75 @@
-hello,you are in index .Here you insert all films or tv series
+@extends("layouts/layout")
+    @section('content')
+    <div id="page">
+        <div class="film">
+            <h3 class="category">Categoria 1</h3>
+            <div class="row">
+                <div class="col-12 col-lg-2">
+                <figure>
+                    <img src="#" alt="Film Title">
+                    <figcaption class="film_title">Titolo</figcaption>
+                </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+            </div><hr />
+            
+            <h3 class="category">Categoria 1</h3>
+            <div class="row">
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+                <div class="col-12 col-lg-2">
+                    <figure>
+                        <img src="#" alt="Film Title">
+                        <figcaption class="film_title">Titolo</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    @endsection
