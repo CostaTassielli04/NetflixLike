1e7bef4577c21797da09f91156d98ca30583cce6
Costantino Tassielli <costantino.tassielli.inf@colamonicochiarulli.edu.it>
2023-03-17 18:53:26 +0100

    all is ready to be retriewed

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -1,14 +1,4 @@
 @extends("layouts/layout")
-<<<<<<< HEAD
-@extends("layouts.app")
-    @section('content')
-    <div id="page">
-        <div class="film">
-            <h3 class="category">Categoria 1</h3>
-            <div class="row">
-            @foreach ($films as $film){
-                <div class="col-12 col-lg-2">
-=======
 @section('content')
     <div class="film">
         <div class="row">
@@ -16,7 +6,6 @@
             @foreach ($films as $film)
 
             <div class="col-12 col-lg-3">
->>>>>>> origin/other_pages
                     <figure>
                         <img src="{{ $film->background_image}}" alt="Film Title"  width="150px" height="200px">
                         <figcaption class="film_title">{{ $film->title }}</figcaption>
@@ -27,8 +16,4 @@
 
         </div>
     </div>
-<<<<<<< HEAD
     @endsection
-=======
-@endsection
->>>>>>> origin/other_pages
